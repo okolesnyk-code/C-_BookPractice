1eb2d42ef40852ed7d966fc2d6225646c9509fd5
oleksii kolesnyk <alexsei081003@gmail.com>
2026-01-13 20:48:27 +0200

    Add code to Chapter2 exercises

---
+++ Chapter2/exercise1.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+void chapter2_exercise1() {
+    SetConsoleCP(1251);        // Кодування для введення з клавіатури (CP1251 – для кирилиці)
+    SetConsoleOutputCP(1251);  // Кодування для виведення на консоль
+
+    const float coefficient = 1.0f / 7.481f;
+    float gallons;
+
+    cout << "Введіть потрібну кількість галонів:: ";
+    cin >> gallons;
+
+    cout << endl << "Еквівалент у кубічних футах: " << gallons * coefficient << endl;
+}
---
+++ Chapter2/exercise3.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+#include <iostream>
+using namespace std;
+
+void chapter2_exercise3() {
+
+	cout << "10" << endl << "20" << endl << "19" << endl;
+
+}

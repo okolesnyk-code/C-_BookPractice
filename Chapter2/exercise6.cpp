07bf4e8dacf14f76f57b0249613f55aa6107be64
oleksii kolesnyk <alexsei081003@gmail.com>
2026-01-14 20:11:27 +0200

    Chapter2: added  exercise6.cpp exercise6.h

---
+++ Chapter2/exercise6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+void chapter2_exercise6() {
+
+	float dollar, pound, frank, german_mark, yen;
+
+	cout << "Enter dollars: ";
+	cin >> dollar;
+
+	pound = dollar / 1.487;
+	frank = dollar / 0.172;
+	german_mark = dollar / 0.584;
+	yen = dollar / 0.00955;
+
+	cout << endl << dollar << "$ = " << pound << " pounds, " << frank << " franks, " << german_mark << " german marks, " << yen << " yens" << endl;
+
+
+}

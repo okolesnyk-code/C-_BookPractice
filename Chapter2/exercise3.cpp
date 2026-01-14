4239b4917f3e883d25c69eda49844df11ece4ca2
oleksii kolesnyk <alexsei081003@gmail.com>
2026-01-14 13:09:57 +0200

    Chapter2: correct exercise3.cpp

--- Chapter2/exercise3.cpp
+++ Chapter2/exercise3.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 void chapter2_exercise3() {
 
-	cout << "10" << endl << "20" << endl << "19" << endl;
+	const int n = 10;
+
+	int n1 = 10, n2 = 10;
+	int n3 = n1 + n2;
+
+	cout << n << endl << n3 << endl << --n3 << endl;
+
 
 }

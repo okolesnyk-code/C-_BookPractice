2c48b7f9007adaaddec5cfb3d8c800487da9c3ef
oleksii kolesnyk <alexsei081003@gmail.com>
2026-01-14 12:48:32 +0200

    Chapter2: exercise4.cpp exercise4.h

---
+++ Chapter2/exercise4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <windows.h>
+#include <iomanip>
+
+using namespace std;
+
+void chapter2_exercise4() {
+
+	SetConsoleCP(65001);
+	SetConsoleOutCP(65001);
+
+
+
+	cout << endl << endl << endl << "\tЯк умру, то поховайте" << endl
+		<< "\tМене на могилі," << endl
+		<< "\tСеред степу широкого," << endl
+		<< "\tНа Вкраїні милій," << endl
+		<< "\tЩоб лани широкополі," << endl
+		<< "\tІ Дніпро, і кручі" << endl
+		<< "\tБуло видно, було чути," << endl
+		<< "\tЯк реве ревучий." << endl
+		<< "\tЯк понесе з України" << endl
+		<< "\tУ синєє море" << endl
+		<< "\tКров ворожу... отоді я" << endl
+		<< "\tІ лани і гори —" << endl
+		<< "\tВсе покину і полину" << endl
+		<< "\tДо самого бога" << endl
+		<< "\tМолитися... А до того —" << endl
+		<< "\tЯ не знаю бога." << endl << endl << endl;
+
+}

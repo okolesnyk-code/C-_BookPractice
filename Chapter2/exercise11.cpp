67dd88bdf97e1210b80cb0daac71e65105a569cb
oleksii kolesnyk <alexsei081003@gmail.com>
2026-01-15 18:21:55 +0200

    Chapter2: added exercise11.cpp exercise11.h

---
+++ Chapter2/exercise11.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void chapter2_exercise11() {
+    cout << setiosflags(ios::left);
+
+    cout << setw(15) << "Surname"
+        << setw(15) << "Name"
+        << setw(20) << "Street"
+        << setw(20) << "City" << endl;
+
+    cout << "---------------------------------------------------------------" << endl;
+
+    cout << setw(15) << "Petrenko"
+        << setw(15) << "Vasyl"
+        << setw(20) << "Shevchenka 10"
+        << setw(20) << "Kyiv" << endl;
+
+    cout << setw(15) << "Ivanenko"
+        << setw(15) << "Serhii"
+        << setw(20) << "Bandery 5"
+        << setw(20) << "Lviv" << endl;
+
+    cout << setw(15) << "Kovalenko"
+        << setw(15) << "Ivan"
+        << setw(20) << "Khreshchatyk 21"
+        << setw(20) << "Dnipro" << endl;
+
+    return 0;
+}

8b0318a40be80d130a94a4ed702af4e3207d78f4
oleksii kolesnyk <alexsei081003@gmail.com>
2026-01-22 17:17:29 +0200

    Chapter3: added exercise4.cpp exercise4.h modified:exercise3.cpp exercise3.h .vcxproj

---
+++ Chapter3/exercise4.cpp
@@ -0,0 +1,49 @@
+#include "exercise4.h"
+#include <iostream>
+
+void chapter3_exercise4()
+{
+    double a, b;
+    char op;
+    char again = 'y';
+
+    while (again == 'y' || again == 'Y')
+    {
+        std::cout << "Enter first operand, operator and second operand: ";
+        std::cin >> a >> op >> b;
+
+        double result = 0.0;
+        bool valid = true;
+
+        switch (op)
+        {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            if (b != 0)
+                result = a / b;
+            else
+            {
+                std::cout << "Error: division by zero\n";
+                valid = false;
+            }
+            break;
+        default:
+            std::cout << "Error: unknown operator\n";
+            valid = false;
+        }
+
+        if (valid)
+            std::cout << "The result is " << result << std::endl;
+
+        std::cout << "Perform another operation (y/n)? ";
+        std::cin >> again;
+    }
+}
--- Chapter3/exercise4.h
+++ Chapter3/exercise4.h
@@ -1,1 +1,3 @@
 #pragma once
+
+void chapter3_exercise4();

34eb58873bf79ef794b6460343815fa6bf747713
oleksii kolesnyk <alexsei081003@gmail.com>
2026-01-22 22:58:42 +0200

    Chapter3: added exercise5.cpp exercise5.h

---
+++ Chapter3/exercise5.cpp
@@ -0,0 +1,21 @@
+#include "exercise5.h"
+#include <iostream>
+
+
+void chapter3_exercise5() {
+
+	int height;
+	std::cout << "Enter height of a pyramid:";
+
+	int symbols_1 = 1; // amount of symbols on the 1 level
+	int symbols_on_the_level; // amount of symbols on the each level
+	int spaces_1 = 1; // amount of spaces on the 1 level
+	int width = height * 2 - 1; //  ()
+
+	for (int i = 0; i < height; i++)
+	{
+
+
+	}
+
+}

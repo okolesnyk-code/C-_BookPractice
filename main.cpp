a06a907117a27694c2c441dffe8238d9d843044f
oleksii kolesnyk <alexsei081003@gmail.com>
2026-01-24 00:06:57 +0200

    main: modified .vcxproj chapter3.h main.cpp

--- main.cpp
+++ main.cpp
@@ -2,10 +2,12 @@
 
 // Підключаємо заголовки меню всіх глав
 #include "exercises/Chapter2/chapter2.h"
+#include "exercises/Chapter3/chapter3.h"
 
 // ... далі підключати інші глави до chapter16.h
 
 int main()
+
 {
     int chapterChoice = -1;
 
@@ -13,6 +15,7 @@ int main()
     {
         std::cout << "\nSelect chapter to view:\n";
         std::cout << "2 - Chapter 2\n";
+        std::cout << "3 - Chapter 3\n";
         //std::cout << "3 - Chapter 3\n";
         // ... додати до 16 глави
         std::cout << "0 - Exit\n";
@@ -23,6 +26,7 @@ int main()
         switch (chapterChoice)
         {
         case 2: chapter2_menu(); break;
+        case 3: chapter3_menu(); break;
         //case 3: chapter3_menu(); break;
             // ... додати до 16 глави
         case 0:
